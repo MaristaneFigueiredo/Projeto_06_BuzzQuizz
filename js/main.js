a98ae17e3500a8cf3b72537c1a40e226754eb9a9
Maristane Figueiredo <maristanetecnologia@gmail.com>
2022-09-01 13:08:05 -0300

    feat: carregando quizz

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
-function toggle(nomeTela, showTela){
+function alternar(nomeTela, showTela){
+    console.log('main-nomeTela',nomeTela)
+    console.log('main-showTela',showTela)
     const componente = document.querySelector(`.${nomeTela}`);
+    console.log('main-componente',componente)
     if (showTela === false) {
         componente.classList.add('escondido');        
     } else componente.classList.remove('escondido');        
+    console.log('main-depois if-componente',componente)
+
 }
+
--- js/tela1.js
+++ js/tela1.js
@@ -1,6 +1,73 @@
-function chamarTela3 () {
-    toggle('tela1',false);
-    toggle('tela2',false);
-    toggle('tela3',true);
+function chamarTela3() {
+    console.log('entrou na função chamarTela3')
+    alternar('tela1', false);
+    alternar('tela2', false);
+    alternar('tela3', true);
+
+}
+
+function chamarTela2() {
+    console.log('entrou na função chamarTela2')
+    alternar('tela1', false)
+    alternar('tela2', true)
+    alternar('tela3', false)
+}
+
+
+iniciarBuzzQuizz();
+
+async function iniciarBuzzQuizz() {
+    const quizzes = await buscarTodosQuizzes()
+    localStorage.setItem('todos-quizzes', JSON.stringify(quizzes))
+    imprimirQuizes(quizzes)
+
+}
+
+function selecionaQuizz(idQuizz) {
+    console.log('idQuizz =', idQuizz)
+    localStorage.setItem('idQuizzSelecionado', idQuizz)
+    chamarTela2()
+    obterQuizzSelecionado(idQuizz)
+}
+
+async function obterQuizzSelecionado(idQuizz) {
+    const { data } = await axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${idQuizz}`)
+    console.log('quiz selecionado =', data)
+    exibeQuizzSelecionado(data)
+
+}
+
+function exibeQuizzSelecionado(quizz) {
+    document.querySelector('.imagem-quizz-selecionado').setAttribute("src", quizz.image)
+
+}
+
+
+
+
+async function buscarTodosQuizzes() {
+    try {
+        const { data } = await axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes')
+        return data
+
+    }
+    catch (exception) {
+        alert('erro')
+    }
+}
+
+function imprimirQuizes(quizzes) {
+    const areaTodosQuizzes = document.querySelector('.todos-quizzes > .area-lista-quizzes')
+    let html = ''
+    for (let i = 0; i < quizzes.length; i++) {
+        // console.log('quizzes[i] =',quizzes[i].title)
+        html += `<div class="quizz" onclick="">
+                    <img class= "formatar-imagem-quizz" src="${quizzes[i].image}" onclick="selecionaQuizz(${quizzes[i].id})">
+                    <div class="titulo-quizz"> ${quizzes[i].title}</div>
+            </div>`
+        areaTodosQuizzes.innerHTML = html
+        //${quizzes[i].id}
+    }
+
 
 }
