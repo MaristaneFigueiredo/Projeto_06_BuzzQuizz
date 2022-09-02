1870df0204886c45be0c747df7d84cc547f753eb
João Paulo S. Amendola <86246910+Permanent5@users.noreply.github.com>
2022-09-02 17:14:35 -0300

    Update tela3.js

--- js/tela3.js
+++ js/tela3.js
@@ -7,12 +7,21 @@ let tela3_1 = document.querySelector('.tela3-1')
 let tela3_2 = document.querySelector('.tela3-2')
 let formulario = document.querySelector('.formulario')
 let contador = 2
+let contadorPerguntas = 1
+let contadorInputTexto1 = 1
+let objeto = {}
+
 
 function prosseguir(){
     nomeDoQuizz = nomeDoQuizz.value
     fotoDoQuizz = fotoDoQuizz.value 
     perguntasQuizz = perguntasQuizz.value
     niveisQuizz = niveisQuizz.value
+
+    objeto = {
+        title: nomeDoQuizz,
+        image: fotoDoQuizz,
+    }
         
     if((nomeDoQuizz.length < 20 || nomeDoQuizz.length > 64)){
         alert('O título deve ter mais de 20 e menos de 60 caracteres')
@@ -43,29 +52,30 @@ function criarFormulario(){
         <img src="./imagens/Vector.png" alt='Botão para abrir a aba' width="26" height="26" onclick="aparecerFormulario(this)">
     </div>
         <div class="opcoesEscondidas${contador+1} escondido">
-            <input type="text" placeholder='Texto da pergunta' required>
-            <input type="text" placeholder="Cor de fundo da pergunta" required>
+            <input type="text" placeholder='Texto da pergunta' class='textoPergunta${contador+1}' required>
+            <input type="text" placeholder="Cor de fundo da pergunta" class='corPergunta${contador+1}' required>
             <h3>Resposta correta</h3>
     
-            <input type="text" placeholder="Resposta correta" required>
-            <input type="url" placeholder="URL da imagem" required>
+            <input type="text" placeholder="Resposta correta" class='respostaCorreta${contador+1}' required>
+            <input type="url" placeholder="URL da imagem" class='UrlPrimeiraIncorreta${contador+1}' required>
             <h3>Respostas incorretas</h3>
     
             <div class="respostaErrada">
-                <input type="text" placeholder="Resposta incorreta 1" required>
-                <input type="url" placeholder="URL da imagem" required>
+                <input type="text" placeholder="Resposta incorreta 1" class='textoIncorreto${contador+1}' required>
+                <input type="url" placeholder="URL da imagem" class='UrlPrimeiraIncorreta${contador+1}' required>
             </div>
     
             <div class="respostaErrada">
-                <input type="text" placeholder="Resposta incorreta 2" required>
-                <input type="url" placeholder="URL da imagem" required>
+                <input type="text" placeholder="Resposta incorreta 2" class='textoIncorreto${contador+1}' required>
+                <input type="url" placeholder="URL da imagem" class='UrlPrimeiraIncorreta${contador+1}' required>
             </div>
     
             <div class="respostaErrada">
-                <input type="text" placeholder="Resposta incorreta 3" required>
-                <input type="url" placeholder="URL da imagem" required>
+                <input type="text" placeholder="Resposta incorreta 3" class='textoIncorreto${contador+1}' required>
+                <input type="url" placeholder="URL da imagem" class='UrlPrimeiraIncorreta${contador+1}' required>
             </div>
     </div>`
+    
     contador += 1
     }
 }
@@ -76,4 +86,16 @@ function aparecerFormulario(imagem){
     divEscondida = divAvo.children[1]
     divEscondida.classList.toggle('escondido')
     console.log(imagem, divPai, divAvo, divEscondida)
+}
+
+
+function preencherFormulario(){
+    objeto.questions =  [
+        {
+        text: document.querySelector(`.opcoesEscondidas1 .textoPergunta1`).value,
+        image: document.querySelector(`.opcoesEscondidas1 .corPergunta1`).value ,
+        
+    }
+    ]
+
 }
