514eef31c1975831008fe1fae1ea3d15f3edbc91
João Paulo Amendola <joaoa@id.uff.br>
2022-09-03 16:15:12 -0300

    finalizacao t3

--- js/tela3.js
+++ js/tela3.js
@@ -9,6 +9,7 @@ let formulario = document.querySelector('.formulario')
 let contador = 2
 let contadorPerguntas = 1
 let contadorInputTexto1 = 1
+let contadorNiveis = 2
 let objeto = {}
 
 
@@ -175,14 +176,67 @@ function preencherFormulario(){
               }
             ]
           }
-        
         )
         contador += 1
-    }
+    } esconderTela3_2()
 }
 
 function esconderTela3_2(){
     let tela3_2 = document.querySelector('.tela3-2')
+    let tela3_3 = document.querySelector('.tela3-3')
     tela3_2.classList.add('escondido')
     tela3_3.classList.remove('escondido')
+    gerarNiveis()
+}
+
+function gerarNiveis(){
+    let telaNiveis = document.querySelector('.tabelaNiveis')
+    while (contadorNiveis < niveisQuizz){
+        telaNiveis.innerHTML += `
+            <div class="nivel${contadorNiveis+1} referenciaNiveis">
+                <h4>Nível ${contadorNiveis+1}</h4>
+                <input type="text" placeholder="Título do nível" class='tituloNivel${contadorNiveis+1}' required>
+                <input type="number" placeholder="Porcentagem de acerto mínima" class='porcentagemNivel${contadorNiveis+1}' required>
+                <input type="url" placeholder="URL da imagem do nível" class='urlNivel${contadorNiveis+1}' required>
+                <input type="text" placeholder="Descrição do nível" class='descricaoNivel${contadorNiveis+1}' required>
+            </div>`
+            contadorNiveis += 1
+    }
+}
+
+function preencherNiveis(){
+    console.log('preencher níveis aqui')
+    contadorNiveis = 2
+    objeto.levels = [
+        {
+            title: document.querySelector('.nivel1 .tituloNivel1').value,
+            image: document.querySelector('.nivel1 .urlNivel1').value,
+            text: document.querySelector('.nivel1 .descricaoNivel1').value,
+            minValue: document.querySelector('.nivel1 .porcentagemNivel1').value
+        },
+        {
+            title: document.querySelector('.nivel2 .tituloNivel2').value,
+            image: document.querySelector('.nivel2 .urlNivel2').value,
+            text: document.querySelector('.nivel2 .descricaoNivel2').value,
+            minValue: document.querySelector('.nivel2 .porcentagemNivel2').value   
+        }
+    ]
+    while (contadorNiveis < niveisQuizz){
+        console.log('entrei no loop dos níveis')
+        console.log(contadorNiveis)
+        objeto.levels.push(
+            {
+            title: document.querySelector(`.nivel${contadorNiveis+1} .tituloNivel${contadorNiveis+1}`).value,
+            image: document.querySelector(`.nivel${contadorNiveis+1} .urlNivel${contadorNiveis+1}`).value,
+            text: document.querySelector(`.nivel${contadorNiveis+1} .descricaoNivel${contadorNiveis+1}`).value,
+            minValue: document.querySelector(`.nivel${contadorNiveis+1} .porcentagemNivel${contadorNiveis+1}`).value
+        }
+    )
+    contadorNiveis += 1
+} 
+}
+
+function esconderTela3_3(){
+    let tela3_3 = document.querySelector('.tela3-3')
+    let tela3_4 = document.querySelector('.tela3-4')
 }
